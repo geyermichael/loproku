761e318fa0a1ff3f325d552113d3396bc7390d77
Michael Geyer <michael@qbitone.de>
2022-03-14 18:38:33 +0100

    add contact page

---
+++ pages/contact/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <section class="px-6 py-32 mx-auto text-center">
+      <h1 class="text-3xl font-bold text-gray-800 dark:text-white md:text-4xl">
+        Contact page
+      </h1>
+    </section>
+  </div>
+</template>
