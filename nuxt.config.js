f1a2260a366f3f833eb499aff56e303a864882ee
Michael Geyer <michael@qbitone.de>
2022-03-14 11:03:52 +0100

    uninstall axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,8 +34,6 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
     // https://github.com/supabase-community/nuxt-supabase
     [
       'nuxt-supabase',
@@ -46,9 +44,6 @@ export default {
       },
     ],
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
